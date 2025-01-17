bf339e21bf36c0e430742dd2ca364b267ddecc01
ramaya947 <ramaya947@yahoo.com>
2025-01-17 14:36:59 -0500

    Updates

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import { FaInstagram, FaLinkedin, FaPhoneAlt } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <footer className="flex h-[100px] flex-row w-full items-center justify-center">
+        <footer className="flex h-[150px] sm:h-[100px] flex-row w-full items-center justify-center border-t-[1px] sm:border-t-0 border-blue-950">
             <div className='hidden flex-1 sm:flex items-center justify-center text-nowrap ml-2 sm:ml-4 sm:font-bold sm:text-xl'>
                 <img src={LogoDesktop} className='h-[50px] sm:h-[50px] md:h-[75px] w-auto' />
             </div>
@@ -20,7 +20,7 @@ export default function Footer() {
                 <a className='flex flex-row w-36 h-10 rounded-lg bg-blue-950 text-white text-center content-center items-center justify-evenly' href='tel:407-927-1480'>
                     <FaPhoneAlt size={15} /> 407-927-1480
                 </a>
-                <a className='w-32 h-10 rounded-lg underline sm:bg-gray-300 text-center content-center' target='_blank' href='https://www.termsfeed.com/live/2a5b3548-ddfa-44bd-9dd3-6738cdf181f0'>Privacy Policy</a>
+                <a className='w-32 h-10 rounded-lg underline sm:no-underline sm:bg-gray-300 text-center content-center' target='_blank' href='https://www.termsfeed.com/live/2a5b3548-ddfa-44bd-9dd3-6738cdf181f0'>Privacy Policy</a>
             </div>
 
         </footer>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,14 +10,19 @@ export default function Home() {
                 {/* Desktop Content */}
                 <div className='hidden sm:flex absolute top-[25%] right-0 w-[50%] h-[30%] bg-white rounded-xl items-center text-blue-950'>
                     <span className='mx-6 sm:text-4xl xl:text-6xl opacity-100 font-sans flex flex-col h-full w-full justify-center items-start'>
-                        Providing<br/>
+                        Providing<br />
                         <div><span className='text-orange-600'>Efficient</span> & <span className='text-orange-600'>Reliable</span></div>
                         <div><span className='text-orange-600'>Structural</span> and <span className='text-orange-600'>Civil Engineering</span></div>
                         Services
                     </span>
                 </div>
             </div>
-
+            <div className='w-full h-[450px] sm:h-[300px] flex flex-col items-center justify-center space-y-2'>
+                <span className='text-2xl font-bold text-blue-950'>WHO WE ARE</span>
+                <span className='text-center mx-6 sm:mx-20'>
+                    Resilience Engineering and Construction Management provides leading structural engineering, façade consulting, integrated design and detailing, forensic, and construction consulting services for all types of buildings. Known for the highest quality of service, the firm provides each client with personal, dedicated, and inspiring engineering services.
+                </span>
+            </div>
         </div>
     )
 }
