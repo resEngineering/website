0bc7a880875d3a3c5d464069a587159d078a7680
ramaya947 <ramaya947@yahoo.com>
2025-03-04 18:13:28 -0500

    Removed About Me Paragraph

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -11,7 +11,7 @@ export default function About() {
                 <div className='w-full sm:w-auto flex flex-1 flex-col text-start sm:justify-center ml-6 sm:ml-0 mt-6 sm:mt-0 space-y-4'>
                     <h1 className='text-blue-950 text-4xl'>Andy Betrand</h1>
                     <h1 className='text-orange-600 text-2xl sm:text-4xl'>President & Founder</h1>
-                    <div className='text-black flex flex-col mr-8 overflow-scroll sm:overflow-auto space-y-2'>
+                    {/* <div className='text-black flex flex-col mr-8 overflow-scroll sm:overflow-auto space-y-2'>
                         <span>I’m a natural born leader with an agility in communication, a fire in my belly and a drive to succeed, all while always trying to improve our collective existence. I’m looking for a position where I will be able to express my leadership and communication talents. I’d like to tell you a little more about myself so that you can better understand my drive and passion.</span>
                         <br/>
                         <span>Raised in a single parent household, my mom worked day and night to give me a private school education. As a kid, out of my group of friends, I was always the one with less and because of that, I wake up every morning with a fiery craving to succeed. My mom showed me that an impeccable work ethic can coexist with, not just personal success but also success for others. As a result of these experiences throughout my early life, I molded myself into the candidate that I am today.</span>
@@ -44,7 +44,7 @@ export default function About() {
                             </li>
                         </ul>
                         <span>I am always looking for new journeys and opportunities feel free to contact me.</span>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
